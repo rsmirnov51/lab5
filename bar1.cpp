5017ab0cf2adc8059ba3d896178a731770dace38
rsmirnov51 <roman-smr@mail.ru>
2017-03-10 14:09:13 +0400

    Rename bar.cpp to bar1.cpp

---
+++ bar1.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+
+using namespace std;
+
+unsigned int Factorial( unsigned int number );
+
+TEST_CASE("Stepen function complete", "[foo]"){
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+}
